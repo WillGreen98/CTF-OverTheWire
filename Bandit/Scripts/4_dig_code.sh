4daaee0b535d28ef03c1333c312283ad56cf2b2b
WillGreen98 <will.green98@hotmail.com>
2019-01-31 01:15:15 +0100

    Script Updates
    Formatting & tidy-up

--- Bandit/Scripts/4_dig_code.sh
+++ Bandit/Scripts/4_dig_code.sh
@@ -1,7 +1,6 @@
 #!/usr/local/bin/bash
 
 # For Level 24 - Brute Force a 4 digit code
-for i in {0000..9999}
-do
+for i in {0000..9999}; do
     echo "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $i" | nc 127.0.0.1 30002
 done
--- Bandit/Scripts/decompressor.sh
+++ Bandit/Scripts/decompressor.sh
@@ -4,27 +4,26 @@
 
 R=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 24)
 
-if [ -f $1 ]
-  then
-    case $(file $1) in
-    *gzip*)
-      echo "gzip!"
-      gzip -d $1 -c > $R
-      ./$0 $R
-      ;;
-    *bzip2*)
-      echo "bzip2!"
-      bzip2 -d $1 -c > $R
-      ./$0 $R
-      ;;
-    *POSIX\ tar*)
-      echo "tar!"
-      tar xOf $1 > $R
-      ./$0 $R
-      ;;
-    *ASCII*)
-      echo "ascii!"
-      cat $1
-      ;;
-    esac
+if [ -f $1 ]; then
+		case $(file $1) in
+		*gzip*)
+			echo "gzip!"
+			gzip -d $1 -c > $R
+			./$0 $R
+			;;
+		*bzip2*)
+			echo "bzip2!"
+			bzip2 -d $1 -c > $R
+			./$0 $R
+			;;
+		*POSIX\ tar*)
+			echo "tar!"
+			tar xOf $1 > $R
+			./$0 $R
+			;;
+		*ASCII*)
+			echo "ascii!"
+			cat $1
+			;;
+		esac
 fi
